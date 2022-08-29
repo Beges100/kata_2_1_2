15570a77b375d3f1be7e1a7987d2efd96081ea55
Beges100 <beges200@gmail.com>
2022-08-29 14:34:27 +0300

    test ok

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld tree =
+                applicationContext.getBean(HelloWorld.class);
+        HelloWorld leaf =
+                applicationContext.getBean(HelloWorld.class);
+
+        Cat one = applicationContext.getBean(Cat.class);
+        Cat two = applicationContext.getBean(Cat.class);
+
+        System.out.println(tree.equals(leaf));
+        System.out.println(one.equals(two));
     }
 }
